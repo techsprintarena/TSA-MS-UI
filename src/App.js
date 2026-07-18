0ef18315d09851cf049481a90d611697a0df534b
hareeswar <hareeswar2003@gmail.com>
2026-07-19 01:51:16 +0530

    techsprint arena initial commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
@@ -6,17 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>
+          TECHSPRINT ARENA IS COMING SOON
+        </h2>
       </header>
     </div>
   );
