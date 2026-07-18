4cfea5acdd93268a14b9676010e251ce3302f088
hareeswar <hareeswar2003@gmail.com>
2026-07-19 01:53:08 +0530

    test cases added

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Techsprint arena coming soon title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/TECHSPRINT ARENA IS COMING SOON/i);
   expect(linkElement).toBeInTheDocument();
 });
